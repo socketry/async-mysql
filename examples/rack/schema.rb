94fbd6ca13b8247d7777c6ca1df6ceb6e64fc6cf
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-04-12 20:54:33 +1200

    Update example and README to be mysql specific.

--- examples/rack/schema.rb
+++ examples/rack/schema.rb
@@ -1,12 +1,15 @@
 
-gem 'pg', '~> 0.18'
+gem 'mysql2'
+
+require 'active_record'
 
 if defined?(Falcon)
 	$stderr.puts "Loading async/mysql"
 	require_relative '../lib/async/mysql'
+	
+	ActiveRecord::Base.establish_connection(adapter: "async_mysql", database: "test", pool: 1024)
+else
+	ActiveRecord::Base.establish_connection(adapter: "mysql2", database: "test", pool: 1024)
 end
 
-require 'active_record'
-
-ActiveRecord::Base.establish_connection(adapter: "mysqlql", database: "test", pool: 1024)
 # ActiveRecord::Base.logger = Logger.new(STDOUT)
